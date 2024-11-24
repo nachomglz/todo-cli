a09a8134b9d9f64f748891ea652f7b1ab965be63
nachomglz <nachopad3@gmail.com>
2024-11-24 19:16:33 +0100

    chore: args boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,44 @@
 use clap::Parser;
+use serde::Serialize;
+
+#[derive(clap::ValueEnum, Clone, Debug, Serialize, Default)]
+enum TodoAction {
+    Add,
+    #[default]
+    View,
+    Complete,
+    Delete,
+}
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    name: String,
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    action: TodoAction,
 }
 
 fn main() {
-    let args = Args::parse();
+    let action = Args::parse().action;
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    match action {
+        TodoAction::Add => add_todo(),
+        TodoAction::View => view_todo(),
+        TodoAction::Complete => complete_todo(),
+        TodoAction::Delete => delete_todo(),
     }
+}
+
+fn add_todo() {
+    todo!()
+}
+
+fn view_todo() {
+    todo!()
+}
+
+fn delete_todo() {
+    todo!()
+}
+
+fn complete_todo() {
+    todo!()
 }

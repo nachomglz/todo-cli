1ead60f5133a0364c53e3d700a710a06c4259b95
nachomglz <nachopad3@gmail.com>
2024-11-29 18:22:15 +0100

    chore: read todos list

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
 use serde::Serialize;
+use std::env;
+use std::fs;
+use std::path::Path;
 
 #[derive(clap::ValueEnum, Clone, Debug, Serialize, Default)]
 enum TodoAction {
@@ -17,7 +20,15 @@ struct Args {
 }
 
 fn main() {
+    let home_dir = env::var("HOME").expect("HOME environment variable not found/set");
+    let home_path = Path::new(&home_dir);
+    let file_path = home_path.join(".todo-cli").join("todo.json");
+
     let action = Args::parse().action;
+
+    let contents = fs::read_to_string(&file_path).expect("The file could not be read");
+
+    // TODO: Parse the contents to an object using Serde
 
     match action {
         TodoAction::Add => add_todo(),
@@ -28,17 +39,21 @@ fn main() {
 }
 
 fn add_todo() {
+    println!("adding todo");
     todo!()
 }
 
 fn view_todo() {
+    println!("viewing todo");
     todo!()
 }
 
 fn delete_todo() {
+    println!("deleting todo");
     todo!()
 }
 
 fn complete_todo() {
+    println!("completing todo");
     todo!()
 }
